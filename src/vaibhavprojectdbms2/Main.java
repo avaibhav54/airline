db7519d65b30ca0c5cadb3eb66d77278e2c67dd9
Vaibhav Agarwal <agarwalvaibhav041@gmail.com>
2020-10-07 21:15:41 +0530

    first

---
+++ src/vaibhavprojectdbms2/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package vaibhavprojectdbms2;
+
+/**
+ *
+ * @author Vaibhav Agarwal
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
